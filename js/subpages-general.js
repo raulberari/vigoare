beb6c6bf74762655e5671d74c69630526b93bb8a
Raul Berari <raulberari@protonmail.com>
2024-06-25 21:46:55 +0300

    Lazy loading

--- js/subpages-general.js
+++ js/subpages-general.js
@@ -1,4 +1,5 @@
 function onLoad() {
+  window.photos = [];
   fetchJSONData();
   storage();
 }
@@ -52,11 +53,9 @@ function shuffleArray(array) {
 }
 
 function initPhotos(descriptions) {
-  const grid = document.getElementById("grid");
   const pageName = document.body.getAttribute("data-name");
 
-  // Add new photos
-  for (let i = 0; i < newPhotos[pageName].length; i++) {
+  function createNewPhoto(i) {
     const img = document.createElement("img");
     const idx = newPhotos[pageName][i];
     const name = pageName + idx;
@@ -119,57 +118,150 @@ function initPhotos(descriptions) {
       });
     }
 
-    grid.appendChild(container);
+    return container;
   }
 
-  // Shuffle the rest of the photos and add them
+  function createNormalPhoto(idx) {
+    const img = document.createElement("img");
+    const name = pageName + idx;
+
+    img.src = `./images/subpages/${pageName}/${pageName}` + idx + ".jpg";
+    img.className = "item";
+    img.alt = descriptions[name] ?? "";
+    img.loading = "lazy";
+
+    let modifiedDescription = descriptions[name] ?? "";
+    modifiedDescription = modifiedDescription.replaceAll(", ", "<br>");
+
+    const description = document.createElement("h1");
+    description.className = "description";
+    description.innerHTML = modifiedDescription;
+    description.lang = "en";
+
+    const descriptionContainer = document.createElement("div");
+    descriptionContainer.className = "description-container";
+    descriptionContainer.style.display = "none";
+    descriptionContainer.appendChild(description);
+
+    const container = document.createElement("div");
+    container.className = "item-container";
+    container.appendChild(descriptionContainer);
+    container.appendChild(img);
+
+    if (modifiedDescription) {
+      container.addEventListener("click", function () {
+        if (descriptionContainer.style.display === "none") {
+          descriptionContainer.style.display = "flex";
+        } else {
+          descriptionContainer.style.display = "none";
+        }
+      });
+    }
+
+    return container;
+  }
+
+  function loadPhotos() {
+    // Add new photos
+    if (loadedPhotosCount === 0) {
+      for (let i = 0; i < newPhotos[pageName].length; i++) {
+        const container = createNewPhoto(i);
+
+        window.photos.push(container);
+      }
+    }
+
+    // Lazy load, a batch at a time
+    const start = loadedPhotosCount;
+    const end = Math.min(
+      loadedPhotosCount + photosPerLoad,
+      photoCount[pageName]
+    );
+
+    // Add the rest of the photos
+    for (let i = start; i < end; i++) {
+      const idx = indexes[i] + 1;
+
+      if (!newPhotos[pageName].includes(idx)) {
+        const container = createNormalPhoto(idx);
+        window.photos.push(container);
+      }
+    }
+
+    loadedPhotosCount = end;
+
+    // Reflow photos into columns
+    reflowPhotos();
+
+    // If all photos are loaded, disconnect the observer
+    if (loadedPhotosCount > photoCount[pageName]) {
+      observer.disconnect();
+    }
+  }
+
+  window.photos = [];
   const indexes = [];
   for (let i = 0; i < photoCount[pageName]; i++) {
     indexes.push(i);
   }
   shuffleArray(indexes);
-  for (let i = 0; i < photoCount[pageName]; i++) {
-    const idx = indexes[i] + 1;
 
-    if (!newPhotos[pageName].includes(idx)) {
-      const img = document.createElement("img");
-      const name = pageName + idx;
+  let loadedPhotosCount = 0;
+  const photosPerLoad = Math.max(10, getColumnCount() * 3);
 
-      img.src = `./images/subpages/${pageName}/${pageName}` + idx + ".jpg";
-      img.className = "item";
-      img.alt = descriptions[name] ?? "";
-      img.loading = "lazy";
-
-      let modifiedDescription = descriptions[name] ?? "";
-      modifiedDescription = modifiedDescription.replaceAll(", ", "<br>");
-
-      const description = document.createElement("h1");
-      description.className = "description";
-      description.innerHTML = modifiedDescription;
-      description.lang = "en";
-
-      const descriptionContainer = document.createElement("div");
-      descriptionContainer.className = "description-container";
-      descriptionContainer.style.display = "none";
-      descriptionContainer.appendChild(description);
-
-      const container = document.createElement("div");
-      container.className = "item-container";
-      container.appendChild(descriptionContainer);
-      container.appendChild(img);
-
-      if (modifiedDescription) {
-        container.addEventListener("click", function () {
-          if (descriptionContainer.style.display === "none") {
-            descriptionContainer.style.display = "flex";
-          } else {
-            descriptionContainer.style.display = "none";
-          }
-        });
+  // Set up Intersection Observer
+  const observer = new IntersectionObserver(
+    (entries) => {
+      if (
+        entries[0].isIntersecting &&
+        loadedPhotosCount < photoCount[pageName]
+      ) {
+        loadPhotos();
       }
-      grid.appendChild(container);
-    }
+    },
+    { threshold: 0.1 }
+  );
+
+  // Observer trigger element
+  const trigger = document.createElement("div");
+  trigger.style.height = "50px";
+  trigger.style.width = "100%";
+  document.body.appendChild(trigger);
+  observer.observe(trigger);
+
+  // Initial load
+  loadPhotos();
+
+  // Reflow photos on window resize or column toggle
+  window.addEventListener("resize", reflowPhotos);
+}
+
+function getColumnCount() {
+  return localStorage.getItem("columnNumber") ?? 1;
+}
+
+function reflowPhotos() {
+  const grid = document.getElementById("grid");
+  // Clear existing columns
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
   }
+
+  // Create new columns
+  const columnCount = getColumnCount();
+  const columns = [];
+  for (let i = 0; i < columnCount; i++) {
+    const column = document.createElement("div");
+    column.className = "grid-column";
+    grid.appendChild(column);
+    columns.push(column);
+  }
+
+  // Distribute photos into columns
+  window.photos.forEach((photo, index) => {
+    const columnIndex = index % columnCount;
+    columns[columnIndex].appendChild(photo);
+  });
 }
 
 function changeColour() {
@@ -208,7 +300,7 @@ function storage() {
   const pageName = document.getElementsByClassName("page-name")[0];
 
   if (localStorage.getItem("columnNumber") > 1) {
-    changeColumns();
+    changeColumns("storage");
   } else {
     document.getElementById("columnsButton").innerText = "1";
   }
@@ -232,7 +324,7 @@ function storage() {
   }
 }
 
-function changeColumns() {
+function changeColumns(origin) {
   const grid = document.getElementById("grid");
   let oneColumnWidth;
 
@@ -244,8 +336,8 @@ function changeColumns() {
     oneColumnWidth = "80%";
   }
 
-  if (grid.style.columnCount > 1) {
-    grid.style.columnCount = 1;
+  if (localStorage.getItem("columnNumber") > 1 && origin !== "storage") {
+    // grid.style.columnCount = 1;
     grid.style.width = oneColumnWidth;
 
     document.getElementById("columnsButton").innerText = "1";
@@ -258,7 +350,7 @@ function changeColumns() {
       numberOfColumns = 2;
     }
 
-    grid.style.columnCount = numberOfColumns;
+    // grid.style.columnCount = numberOfColumns;
     grid.style.width = "90%";
 
     document.getElementById("columnsButton").innerText =
@@ -266,6 +358,8 @@ function changeColumns() {
 
     localStorage.setItem("columnNumber", numberOfColumns);
   }
+
+  reflowPhotos();
 }
 
 function onResize() {
@@ -274,6 +368,7 @@ function onResize() {
     numberOfColumns = 2;
   }
   const grid = document.getElementById("grid");
+  const columnButton = document.getElementById("columnsButton");
 
   if (window.innerWidth > 1280) {
     oneColumnWidth = "45%";
@@ -283,9 +378,12 @@ function onResize() {
     oneColumnWidth = "80%";
   }
 
-  if (grid.style.columnCount > 1) {
-    grid.style.columnCount = numberOfColumns;
+  if (localStorage.getItem("columnNumber") > 1) {
+    // grid.style.columnCount = numberOfColumns;
+    columnButton.innerText = numberOfColumns.toString();
+    localStorage.setItem("columnNumber", numberOfColumns);
   } else {
     grid.style.width = oneColumnWidth;
+    localStorage.setItem("columnNumber", 1);
   }
 }
